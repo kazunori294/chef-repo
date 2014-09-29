94b593ab81942a0187c5a1f54a523d6089a5fb53
root <root@ip-172-31-14-232.ap-northeast-1.compute.internal>
2014-09-29 06:26:35 +0000

    test

--- solo.rb
+++ solo.rb
@@ -1,5 +1,5 @@
 file_cache_path "/tmp/chef-solo"
-cookbook_path ["/root/chef-repo/mycookbooks", "/root/chef-repo/cookbooks"]
-role_path "/root/chef-repo/roles"
+cookbook_path ["/etc/chef-repo/mycookbooks", "/etc/chef-repo/cookbooks"]
+role_path "/etc/chef-repo/roles"
 
 
