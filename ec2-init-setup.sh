b5d316b692745af3eb5987b6cc78b3653488126e
kazunori294 <kazunori294@gmail.com>
2014-10-06 08:20:30 +0900

    Update ec2-init-setup.sh

--- ec2-init-setup.sh
+++ ec2-init-setup.sh
@@ -21,3 +21,103 @@ echo -e "Host github.com\n\tStrictHostKeyChecking no\n" >> /root/.ssh/config
 git clone git@github.com:kazunori294/chef-repo.git /etc/chef-repo  >> /tmp/init.log
 
 chef-solo -c /etc/chef-repo/solo.rb -j /etc/chef-repo/nodes/localhost.json
+
+
+
+#!/usr/bin/ruby 
+
+require 'rubygems'
+require 'aws-sdk'
+
+ec2 = AWS::EC2.new(
+  :ec2_endpoint => 'ec2.ap-northeast-1.amazonaws.com',
+  :access_key_id => '',
+  :secret_access_key => ''
+)
+
+ec2.instances.each {|ins|
+  p ins.id
+}
+
+
+
+#!/usr/bin/ruby 
+
+require 'rubygems'
+require 'aws-sdk'
+
+
+subnet = 'subnet-eb3108ad'
+
+image_id = 'ami-35072834' ### Amazon Linux
+instance_type = 't2.micro'
+key_name = 'myaws-key'
+security_group_ids = [
+  'sg-c97e8fac', ### default
+]
+#iam_instance_profile = 'default'
+ebs_size = 8
+
+
+ec2 = AWS::EC2.new(
+  :ec2_endpoint => 'ec2.ap-northeast-1.amazonaws.com',
+  :access_key_id => '',
+  :secret_access_key => ''
+)
+
+instance = ec2.instances.create(
+  image_id:        image_id,
+  instance_type:   instance_type,
+  key_name:        key_name,
+  subnet:          subnet,
+  security_group_ids: security_group_ids,
+  #iam_instance_profile: iam_instance_profile,
+  block_device_mappings: [
+    {
+      device_name: '/dev/xvda',
+      ebs: { volume_size: ebs_size }
+    }
+  ]
+)
+
+while instance.status != :running
+  puts "Launching instance #{instance.id}, status: #{instance.status}"
+  sleep 5
+end
+
+
+
+#!/usr/bin/ruby 
+
+require 'rubygems'
+require 'aws-sdk'
+
+
+subnet = 'subnet-eb3108ad'
+
+image_id = 'ami-35072834' ### Amazon Linux
+instance_type = 't2.micro'
+key_name = 'myaws-key'
+security_group_ids = [
+  'sg-c97e8fac', ### default
+]
+#iam_instance_profile = 'default'
+ebs_size = 8
+
+
+ec2 = AWS::EC2.new(
+  :ec2_endpoint => 'ec2.ap-northeast-1.amazonaws.com',
+  :access_key_id => '',
+  :secret_access_key => ''
+)
+
+instance = ec2.instances['i-a91e8fb0']
+instance.terminate
+
+#while instance.status != :running
+#  puts "Launching instance #{instance.id}, status: #{instance.status}"
+#  sleep 5
+#end
+
+
+
